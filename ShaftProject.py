a1f6eb09b96de4888387d1039c8590db5d5c08c3
ethanlawhead <ethanlawhead@hotmail.com>
2024-02-22 17:54:04 -0700

    function of code completed

--- ShaftProject.py
+++ ShaftProject.py
@@ -1,4 +1,5 @@
 import math
+
 # 1030 HR Steel info
 sultmet = 470_000_000
 sulteng = 68
@@ -9,14 +10,8 @@ seprime_eng = sulteng/2
 
 root_a_bending = 0.246-(3.08*10**(-3)*sulteng)+(1.51*10**(-5)*sulteng**2)-(2.67*10**(-8)*sulteng**3)
 root_a_torsion = 0.190-(2.51*10**(-3)*sulteng)+(1.35*10**(-5)*sulteng**2)-(2.67*10**(-8)*sulteng**3)
-
-
-
-bend_mom = int(input('what is the bending moment'))
-torque = int(input('what is the torque'))
-diameter = float(input('what is the diameter'))
-criteria = input('what criteria? (g, vm, c, cig)')
-kt_criteria= input('what is stress concentration?')
+run = True
+    
 
 def stress_concentration(diameter,kt_criteria):
 
@@ -45,44 +40,48 @@ def stress_concentration(diameter,kt_criteria):
     kfs = 1 + ((kts - 1)/(1 + (root_a_torsion/math.sqrt(r))))
     
     return kf,kfs
-print(stress_concentration(1,kt_criteria))
-#kf
-print(stress_concentration(1,kt_criteria)[0])
-print(stress_concentration(diameter,kt_criteria))
 
 
 #se
-ka = 2*(sulteng**(-.217))
+def se(diameter,sult,seprime):
 
-if diameter <= 2 and diameter != 0:
-    kb = 0.879 * (diameter**(-0.107))
-elif diameter > 2:
-    kb = 0.91*(diameter**(-.157))
-else:
-    kb = 1
+    ka = 2*(sult**(-.217))
 
+    if diameter <= 2 and diameter != 0:
+        kb = 0.879 * (diameter**(-0.107))
+    elif diameter > 2:
+        kb = 0.91*(diameter**(-.157))
+    else:
+        kb = 1
+    
+    se = seprime * ka * kb
 
-se = seprime_eng * ka * kb
-def sigma_prime(diameter,ma,tm)
-    sigmaa_prime = math.sqrt((((32*(stress_concentration(diameter)[0])*ma)/(math.pi*(diameter**3)))**2))
-    sigmam_prime = math.sqrt(3*(((16*(stress_concentration(diameter)[1])*tm)/(math.pi*(diameter**3)))**2))
+    return se
+
+def sigma_prime(diameter,ma,tm,kt_criteria):
+
+    sigmaa_prime = math.sqrt((((32*(stress_concentration(diameter,kt_criteria)[0])*ma)/(math.pi*(diameter**3)))**2))
+    sigmam_prime = math.sqrt(3*(((16*(stress_concentration(diameter,kt_criteria)[1])*tm)/(math.pi*(diameter**3)))**2))
     return (sigmaa_prime,sigmam_prime)
-#goodman criteria 
-def goodman(d,kf,kfs,ma,tm,se, sult):
 
+#goodman criteria 
+
+def goodman(d, kf, kfs, ma, tm, sult,seprime):
+
+    goodman_se = se(d,sult,seprime)
     # finding A
     a = math.sqrt((4*((kf*ma)**2)))
     #finding B
     b = math.sqrt((3*((kfs*tm)**2)))
-    safety = ((math.pi*(d**3))/16) * (((a/(se*1000))+(b/(sult*1000)))**(-1))
-    print(safety)
+    safety = ((math.pi*(d**3))/16) * (((a/(goodman_se*1000))+(b/(sult*1000)))**(-1))
     return safety
-#goodman(diameter,kf,kfs,bend_mom,torque,se,sulteng)
+
+#print(goodman(diameter,stress_concentration(diameter,kt_criteria)[0],stress_concentration(diameter,kt_criteria)[1],bend_mom,torque,goodman_se,sulteng))
 
 def von_mises(kf,kfs,sy,ma,tm,d):
+
     sigma_max = math.sqrt((((32*kf*ma)/(math.pi*(d**3)))**2)+(3*(((16*kfs*tm)/(math.pi*(d**3)))**2)))
     safety = (sy/sigma_max)
-    print(safety)
     return safety
 
 #von_mises(kf,kfs,sy,bend_mom,torque,diameter)
@@ -90,25 +89,61 @@ def von_mises(kf,kfs,sy,ma,tm,d):
 def conservative(sigmaM,sigmaA,sy):
 
     safety = sy/(sigmaM + sigmaA)
-    print(safety)
     return safety
 
 #conservative(sigmam_prime,sigmaa_prime,sy)
     
-def cig(sigmaM,SigmaA,sy,kf,kfs,ma,tm,se,sult):
-    good_diameter = 0.000
+def cig(sy,ma,tm,sult,kt_criteria,seprime):
+
+    good_diameter = 0
     conserve_diameter = 0
-    safety_factor = 1.5
     good_safe = 0
     conserve_safe = 0
-    while good_safe != safety_factor:
+
+    while good_safe < 1.5:
         good_diameter += 0.001
-        good_safe = goodman(diameter,kf,kfs,ma,tm,se,sult)
+        kf = stress_concentration(good_diameter,kt_criteria)[0]
+        kfs = stress_concentration(good_diameter,kt_criteria)[1]
 
-    while conserve_safe != safety_factor:
+        good_safe = goodman(good_diameter,kf,kfs,ma,tm,sult,seprime)
+
+    while conserve_safe < 1.5:
         conserve_diameter += 0.001
-        conservative(,,sy)
+        sigmaA = sigma_prime(conserve_diameter,ma,tm,kt_criteria)[0]
+        sigmaM = sigma_prime(conserve_diameter,ma,tm,kt_criteria)[1]
+        conserve_safe =conservative(sigmaA,sigmaM,sy)
 
-    print(diameter)
+    if conserve_diameter > good_diameter:
+        return conserve_diameter
+    else:
+        return good_diameter
 
-cig(sigma_prime[0],sigma_prime[1],sy,kf,kfs,bend_mom,torque,se,sulteng)
+#cig(sy,bend_mom,torque,sulteng,kt_criteria,seprime_eng)
+        
+
+while run == True:
+    bend_mom = int(input('what is the bending moment'))
+    torque = int(input('what is the torque'))
+
+    criteria = input('what criteria? (g, vm, c, cig)')
+    kt_criteria= input('what is stress concentration?')
+    
+    if criteria != 'cig':
+        diameter = float(input('what is the diameter'))
+    
+    if criteria =='g':
+        print(goodman(diameter,stress_concentration(diameter,kt_criteria)[0],stress_concentration(diameter,kt_criteria)[1],bend_mom,torque,sulteng,seprime_eng))
+    elif criteria =='vm':
+        print(von_mises(stress_concentration(diameter,kt_criteria)[0],stress_concentration(diameter,kt_criteria)[1],sy,bend_mom,torque,diameter))
+    elif criteria =='c':
+        print(conservative(sigma_prime(diameter,bend_mom,torque,kt_criteria)[1],sigma_prime(diameter,bend_mom,torque,kt_criteria)[0],sy))
+
+    elif criteria == 'cig':
+        print(cig(sy,bend_mom,torque,sulteng,kt_criteria,seprime_eng))
+    
+    runagain = input('would you like to solve another? (y/n)')
+
+    if runagain =='n':
+        run = False
+    else:
+        run = True
